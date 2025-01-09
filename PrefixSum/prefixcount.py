00e633366100629d54b013c64e83f8c39bdce0d4
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2025-01-09 20:08:55 +0530

    Create prefixcount.py

---
+++ PrefixSum/prefixcount.py
@@ -0,0 +1,8 @@
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        count=0
+        for i in range(len(words)):
+            if words[i][0:len(pref)]==pref:
+                count+=1
+        return count
+        
